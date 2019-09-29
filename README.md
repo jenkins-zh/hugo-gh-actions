600fdabb12ffb0c82307557e06b4dd3ea56beb09
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-09-29 11:09:22 +0800

    Project init commit

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# hugo-gh-actions
+# Hugo Github Action
+
+GitHub Action for building and publishing Hugo-built site.
+
+Inspired by [khanhicetea/gh-actions-hugo-deploy-gh-pages](https://github.com/khanhicetea/gh-actions-hugo-deploy-gh-pages)
+
+## Secrets
+
+- `GIT_DEPLOY_KEY` - *Required* your deploy key which has **Write access**
+
+## Create Deploy Key
+
+1. Generate deploy key `ssh-keygen -t rsa -f hugo -q -N ""`
+1. Then go to "Settings > Deploy Keys" of repository
+1. Add your public key within "Allow write access" option.
+1. Copy your private deploy key to `GIT_DEPLOY_KEY` secret in "Settings > Secrets"
+
+## Environment Variables
+
+- `HUGO_VERSION` : **optional**, default is **0.54.0** - [check all versions here](https://github.com/gohugoio/hugo/releases)
+
+```
+name: Deploy to GitHub Pages
+
+on:
+  push:
+    branches:
+    - master
+
+jobs:
+  hugo-deploy-gh-pages:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@master
+    - name: hugo-deploy-gh-pages
+      uses: linuxsuren/surenpi@master
+      env:
+        GIT_DEPLOY_KEY: ${{ secrets.GIT_DEPLOY_KEY }}
+        HUGO_VERSION: "0.53"
+        WEBSITE_GIT: "git@github.com:LinuxSuRen/linuxsuren.github.io.git"
+        GIT_USER: ""
+        GIT_EMAIL: ""
+```
---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+echo '=================== Install Hugo ==================='
+DOWNLOAD_HUGO_VERSION=${HUGO_VERSION:-0.54.0}
+echo "Installing Hugo $DOWNLOAD_HUGO_VERSION"
+wget -O /tmp/hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${DOWNLOAD_HUGO_VERSION}/hugo_extended_${DOWNLOAD_HUGO_VERSION}_Linux-64bit.tar.gz &&\
+tar -zxf /tmp/hugo.tar.gz -C /tmp &&\
+mv /tmp/hugo /usr/local/bin/hugo &&\
+rm /tmp/*
+
+echo '=================== Create deploy key to push ==================='
+mkdir /root/.ssh
+ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts && \
+echo "${GIT_DEPLOY_KEY}" > /root/.ssh/id_rsa && \
+chmod 400 /root/.ssh/id_rsa
+
+echo '=================== Clone website ==================='
+git clone ${WEBSITE_GIT}
+
+echo '=================== Build site ==================='
+hugo
+ls -hal
+echo '=================== Publish to GitHub Pages ==================='
+cp -R public/* linuxsuren.github.io
+cd linuxsuren.github.io && \
+git config user.name "${GITHUB_ACTOR}" && \
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
+git log -3 && \
+git add . && \
+pwd && \
+git remote -vv && \
+echo -n 'Files to Commit:' && ls -l | wc -l && \
+timestamp=$(date +%s%3N) && \
+git commit -m "Automated deployment to GitHub Pages on $timestamp" && \
+git status && \
+git push origin master --force
+echo '=================== Done  ==================='
